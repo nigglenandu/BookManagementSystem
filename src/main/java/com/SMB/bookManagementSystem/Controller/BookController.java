c425a74e4874a005ff8b042346dcffa67deddbdc
[Niggle Nandu] <nigglenandu@gmail.com>
2025-02-22 23:15:41 +0545

    openapi

--- src/main/java/com/SMB/bookManagementSystem/Controller/BookController.java
+++ src/main/java/com/SMB/bookManagementSystem/Controller/BookController.java
@@ -2,6 +2,8 @@ package com.SMB.bookManagementSystem.Controller;
 
 import com.SMB.bookManagementSystem.Entity.Book;
 import com.SMB.bookManagementSystem.Service.IServiceBook;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,10 +14,13 @@ import java.util.Optional;
 
 @Controller
 @RequestMapping("books")
+@Tag(name = "Book Management", description = "Operations related to books")
 public class BookController {
     @Autowired
     public IServiceBook serviceBook;
 
+
+    @Operation(summary = "Get all books")
     @GetMapping
     public String getAllBooks(Model model) {
         List<Book> books = serviceBook.getAllBooks();
@@ -23,6 +28,7 @@ public class BookController {
         return "books";
     }
 
+    @Operation(summary = "Get books by id")
     @GetMapping("/searchById")
     public String getBookById(@RequestParam("id") Long id, Model model) {
         Optional<Book> bookOpt = serviceBook.getBookById(id);
@@ -36,6 +42,7 @@ public class BookController {
         }
     }
 
+    @Operation(summary = "Get books by title")
     @GetMapping("/searchByTitle")
     public String getBooksByTitle(@RequestParam("title") String title, Model model){
         List<Book> books = serviceBook.getAllBooksByTitle(title);
@@ -46,18 +53,22 @@ public class BookController {
             return "notfound";
         }
     }
+
+    @Operation(summary = "Book form")
     @GetMapping("/create")
     public String showAddBookForm(Model model){
         model.addAttribute("book", new Book());
         return "add-book";
     }
 
+    @Operation(summary = "Create Book")
     @PostMapping("/create")
     public String createBook(@ModelAttribute Book book){
         serviceBook.addBook(book);
         return "redirect:/books";
     }
 
+    @Operation(summary = "Update form")
     @GetMapping("update/{id}")
     public String showUpdateBookForm(@PathVariable Long id, Model model){
         Optional<Book> bookOpt = serviceBook.getBookById(id);
@@ -69,13 +80,15 @@ public class BookController {
         }
     }
 
+    @Operation(summary = "Update Book by id")
     @PostMapping("update/{id}")
     public String updateBookById(@PathVariable Long id, @ModelAttribute Book book){
         serviceBook.updateBookById(id, book);
         return "redirect:/books";
     }
 
-    @GetMapping("delete/{id}")
+    @Operation(summary = "Delete book by id")
+    @DeleteMapping("delete/{id}")
     public String deleteById(@PathVariable Long id){
         serviceBook.deleteBookById(id);
         return "redirect:/books";
